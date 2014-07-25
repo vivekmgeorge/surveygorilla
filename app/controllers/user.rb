ddb550827280883f5bdca4b47eadce51dc258f6e
Stella Kim <stellajkimm@gmail.com>
2014-07-25 15:27:12 -0400

    finished users

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -33,5 +33,14 @@ get '/user/logout' do
 end
 # take user to thier homepage
 get "/user/home" do
+  @user = User.find(session[:user_id])
+  @surveys_created = @user.surveys
+
+  @responses = Response.where(user_id: session[:user_id])
+  @surveys_taken = []
+  @responses.each do |response|
+    @surveys_taken << Survey.find(Question.find(response.question_id).survey_id)
+  end
+  @surveys_taken.uniq!
   erb :"user/home"
 end
