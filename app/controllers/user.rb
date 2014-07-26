49ebdb29ed44b0b226003f9ed5e4fd5951fada97
Stella Kim <stellajkimm@gmail.com>
2014-07-25 20:04:53 -0400

    merging

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -7,7 +7,7 @@ end
 # will direct the user to their homepage after signing in
 post "/user" do
   @user = User.find_by(email: params[:user][:email])
-  if @user && @user.authenticate(password: params[:user][:password])
+  if @user && @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
     redirect "/user/home"
   else
@@ -45,7 +45,4 @@ get "/user/home" do
   erb :"user/home"
 end
 
-# delete '/survey/:id' do
-#   Survey.find(params[:id]).destroy
-#   redirect "/user/home"
-# end
+
